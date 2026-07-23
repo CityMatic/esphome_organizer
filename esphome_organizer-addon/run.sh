77bd18910f05ad8925e24842f6549919a5e38130
CityMatic <149718958+CityMatic@users.noreply.github.com>
2026-07-23 22:56:12 +0200

    Initialize run script for ESPHome Organizer
    Add a simple HTTP server to run ESPHome Organizer.

--- esphome_organizer-addon/run.sh
+++ esphome_organizer-addon/run.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+echo "=== ESPHome Organizer start ==="
+
+# Start serwera panelu (prosty serwer HTTP)
+cd /www
+python3 -m http.server 8099 &
+
+# Główna pętla
+while true; do
+    sleep 60
+done
 
